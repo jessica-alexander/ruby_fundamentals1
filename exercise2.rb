2dab022c9d23090467f65193a8ab47a154bf61cc
Jessica A <jhai_alex@hotmail.com>
2016-10-18 14:37:02 -0400

    completed exercise 2

--- exercise2.rb
+++ exercise2.rb
@@ -1,1 +1,9 @@
 puts "A good tip for $55.00 is $#{55 * 0.150}"
+
+candy = 5
+chocolate ='7'
+puts "#{candy.to_s + chocolate}"
+
+puts "The answer your looking for is #{45628 * 7839}."
+
+puts "#{(10 < 20 && 30 < 20) || !(10 == 11)}"
